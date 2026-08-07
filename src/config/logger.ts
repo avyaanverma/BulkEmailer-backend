099ba03e3d6f5a48e1019bf1bb366c2faf49ced6
avyaanverma <avyaanver@gmail.com>
2026-08-07 16:41:38 +0530

    logger util added

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,8 +1,18 @@
 import pino from "pino";
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || "debug",
-  transport: {
-    target: "pino-pretty",
-  },
-});
+const transport =
+  config.node_env === "development"
+    ? pino.transport({
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid, hostname",
+        },
+      })
+    : undefined;
+
+export const logger = pino(
+  { level: config.node_env === "development" ? "debug" : "info" },
+  transport,
+);
