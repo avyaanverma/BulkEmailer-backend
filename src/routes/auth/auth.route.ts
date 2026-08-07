7dbb4024678aebdd35d14cf09ecd2b6eb6032f7c
avyaanverma <avyaanver@gmail.com>
2026-08-08 00:02:46 +0530

    authentication with google routes added

---
+++ src/routes/auth/auth.route.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import AuthController from "../../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/google", asyncHandler(AuthController.googleLogin));
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import healthRoutes from "./health/health.route.js";
+import authRoutes from "./auth/auth.route.js";
+
 const router = Router();
 
 router.use("/health", healthRoutes);
-
+router.use("/auth", authRoutes);
 export default router;
